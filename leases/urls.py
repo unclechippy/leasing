d677cfd003db0feb99427740e574fac3c7efd03d
unclechippy <jasonlapoint@gmail.com>
2018-08-14 12:14:25 -0700

    add urls for lease and charge

--- leases/urls.py
+++ leases/urls.py
@@ -7,4 +7,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:lease_id>', views.lease, name='lease'),
+    path('<int:charge_id>', views.charge, name='charge')
 ]
