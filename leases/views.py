6b2b2641877a8014f199ffc148b99ad368a95b27
unclechippy <jasonlapoint@gmail.com>
2018-08-13 17:02:42 -0700

    add lease view

--- leases/views.py
+++ leases/views.py
@@ -5,3 +5,14 @@ from django.template import loader
 def index(request):
     template = loader.get_template('lease.html')
     return HttpResponse(template.render({}, request))
+
+def lease(request, lease_id)
+    try:
+        lease = Lease.objects.get(pk=lease_id)
+    except:
+        Lease.DoesNotExist:
+            raise Http404("Lease does not exist")
+    context = {
+        "lease" : lease
+    }
+    return render(request, "leases/lease.html", context)
