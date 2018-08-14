a17d415261dd59edcb9b93c6a1493e7679feb867
jasonlapoint <jasonlapoint@gmail.com>
2018-08-13 23:12:25 -0700

    added ChargeDetail
    Signed-off-by: jasonlapoint <jasonlapoint@gmail.com>

--- leases/models.py
+++ leases/models.py
@@ -13,9 +13,12 @@ class Lease(models.Model):
 class Charge(models.Model):
     lease = models.ForeignKey(Lease, on_delete=models.CASCADE)
     name = models.CharField(max_length=CHAR_FIELD_LEN)
-    amount = models.DecimalField(max_digits=19, decimal_places=10)
-    start_date = models.DateField()
-    end_date = models.DateField()
 
     def __str__(self):
         return "Charges for %s" % self.name
+
+class ChargeDetail(models.Model):
+    charge = models.ForeignKey(Charge, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=19, decimal_places=10)
+    start_date = models.DateField()
+    end_date = models.DateField()
