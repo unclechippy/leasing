835f57f1e02dca50e895d00a0cd0816fa98e41e5
jasonlapoint <jasonlapoint@gmail.com>
2018-08-14 23:06:10 -0700

    various updates
    Signed-off-by: jasonlapoint <jasonlapoint@gmail.com>

--- leases/urls.py
+++ leases/urls.py
@@ -6,7 +6,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.dashboard, name='dashboard'),
+    path('new', views.new_lease, name='new_lease'),
     path('<int:lease_id>', views.lease, name='lease'),
     path('<int:charge_id>', views.charge, name='charge')
 ]
--- leases/views.py
+++ leases/views.py
@@ -1,18 +1,25 @@
 from django.http import HttpResponse
 from django.template import loader
 
-
-def index(request):
+#dashboard
+def dashboard(request):
     template = loader.get_template('lease.html')
     return HttpResponse(template.render({}, request))
 
-def lease(request, lease_id)
-    try:
-        lease = Lease.objects.get(pk=lease_id)
-    except:
-        Lease.DoesNotExist:
-            raise Http404("Lease does not exist")
+def lease(request, lease_id):
+    #try:
+    #    lease = Lease.objects.get(pk=lease_id)
+    #except Lease.DoesNotExist:
+    #        raise Http404("Lease does not exist")
+    lease = get_object_or_404(Lease, pk=lease_id)
     context = {
         "lease" : lease
     }
     return render(request, "leases/lease.html", context)
+
+def new_lease(request):
+    template = loader.get_template('new_lease.html')
+    return HttpResponse(template.render({}, request))
+
+def charge(request, charge_id):
+    pass
